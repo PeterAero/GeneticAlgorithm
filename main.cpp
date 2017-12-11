97f88e5151b41f02f8e84eb21466d49f643df95e
Peter Fischer <Peter.Fischer@dlr.de>
2017-12-11 13:03:36 +0100

    switch to calculator

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+int calc_add(int a, int b){
+
+    return a + b;
+
+}
+
 int main(int argc, char *argv[])
 {
-    std::cout << "Added new Branch!" << std::endl;
+    int a, b, c = 0;
+    a = 3;
+    b = 4;
+    c = calc_add(a, b);
+    std::cout << "Result is: " << c << std::endl;
     return 0;
 }
